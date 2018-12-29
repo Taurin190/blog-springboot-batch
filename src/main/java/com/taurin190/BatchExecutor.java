ab916c83b1806cff06bf870677a377d96e35caca
Koichi Taura <taura.koichi@gmail.com>
2018-12-29 23:52:55 +0900

    modify to select batch controller in application class

--- src/main/java/com/taurin190/BatchExecutor.java
+++ src/main/java/com/taurin190/BatchExecutor.java
@@ -2,46 +2,48 @@ package com.taurin190;
 
 import com.taurin190.controller.BatchController;
 import com.taurin190.controller.AuthorController;
+import com.taurin190.controller.BlogController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 
 
-@SpringBootApplication
-public class BatchExecutor implements CommandLineRunner {
-    @Autowired
-    private ApplicationContext context;
-
-    @Autowired
-    private Environment env;
-
-    @Qualifier("com.taurin190.controller.BlogController")
-    @Autowired
-    private BatchController batchController;
+@SpringBootApplication(scanBasePackages = {"com.taurin190"})
+public class BatchExecutor implements CommandLineRunner{
 
     private static final Logger log = LoggerFactory.getLogger(BatchExecutor.class);
 
+    @Autowired
+    AuthorController authorController;
+
+    @Autowired
+    BlogController blogController;
+
+    BatchController batchController;
+
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(BatchExecutor.class);
-        ApplicationContext context = application.run();
+        ApplicationContext context = application.run(args);
         SpringApplication.exit(context);
     }
+
     @Override
     public void run(String... args) throws Exception {
-//        Class<?> cls = Class.forName("AuthorController");
-//        BatchController batchController = (BatchController) cls.newInstance();
-//        BatchController batchController = (BatchController) new AuthorController();
+        switch (args[0]) {
+            case "author":
+                batchController = authorController;
+                break;
+            case "blog":
+                batchController = blogController;
+                break;
+            default:
+                batchController = blogController;
+                break;
+        }
         batchController.exec();
     }
-
-
 }
