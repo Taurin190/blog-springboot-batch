1cbe70233f32b8bcd180dd9006e26c8683974561
Koichi Taura <taura.koichi@gmail.com>
2019-01-15 22:36:35 +0900

    modify test with modification to remove body tag

--- src/test/java/com/taurin190/utils/BlogUtilTest.java
+++ src/test/java/com/taurin190/utils/BlogUtilTest.java
@@ -13,9 +13,9 @@ public class BlogUtilTest {
     @Test
     public void getHTMLBodyByEnglishTitle() {
         String html = BlogUtil.getHTMLBodyByEnglishTitle("create_blog_site");
-        assertEquals("<body> \n" +
+        assertEquals(" \n" +
                 " <h2>はじめに</h2> \n" +
                 " <p>アウトプットの重要性を感じ、ブログサイトを作成しました。 Qiitaやはてブを使わずに一から作成することで、一から作れるかを確かめられ、 学び直しと次の課題を考えることが出来ました。 仕事から帰ってからや休日などの時間でSpringbootのアプリをGKE上に置いて公開しました。 </p>  \n" +
-                "</body>", html);
+                "", html);
     }
 }
