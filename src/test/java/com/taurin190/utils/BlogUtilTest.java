dea1f710696788f5f087ddb0de2c2a9f3d0c9952
Taurin190 <taura.koichi@gmail.com>
2019-01-16 14:03:06 +0900

    add getJsonList test

--- src/test/java/com/taurin190/utils/BlogUtilTest.java
+++ src/test/java/com/taurin190/utils/BlogUtilTest.java
@@ -1,13 +1,26 @@
 package com.taurin190.utils;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class BlogUtilTest {
     @Test
     public void getBlogJSONObjectList() {
-
+        List<JSONObject> jsonObjectList = BlogUtil.getBlogJSONObjectList();
+        assertEquals(2, jsonObjectList.size());
+        try {
+            assertEquals("test", jsonObjectList.get(0).getString("title"));
+            assertEquals("/", jsonObjectList.get(0).getString("directory"));
+            assertEquals("hoge", jsonObjectList.get(1).getString("title"));
+            assertEquals("/2018/", jsonObjectList.get(1).getString("directory"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
