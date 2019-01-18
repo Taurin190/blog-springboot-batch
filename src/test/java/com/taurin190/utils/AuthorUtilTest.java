2f80fd10e525ce5390d962cf91881795ba77b866
Taurin190 <taura.koichi@gmail.com>
2019-01-18 16:47:18 +0900

    add test case of authorutil

--- src/test/java/com/taurin190/utils/AuthorUtilTest.java
+++ src/test/java/com/taurin190/utils/AuthorUtilTest.java
@@ -1,5 +1,29 @@
 package com.taurin190.utils;
 
-public class AuthorUtilTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class AuthorUtilTest {
+    @Test
+    public void getProperties() {
+        String authorName = AuthorUtil.getProperty("author_name");
+        String selfIntroduction = AuthorUtil.getProperty("self_introduction");
+        String twitterAccount = AuthorUtil.getProperty("twitter_account");
+        String githubAccount = AuthorUtil.getProperty("github_account");
+
+        assertEquals("Koichi Taura",authorName);
+        assertEquals("東京でソフトウェアエンジニアとして働いていて。 PHP, Java, Pythonでバックエンドの開発を行っています。 Docker/Kubernetesへの環境構築移行も行なっております。",selfIntroduction);
+        assertEquals("https://twitter.com/Tauitter51",twitterAccount);
+        assertEquals("https://github.com/Taurin190",githubAccount);
+    }
+
+    @Test
+    public void getPropertiesWithDefaultValues() {
+        String authorName = AuthorUtil.getProperty("author_name", "Test Taro");
+        String facebookAccount = AuthorUtil.getProperty("facebook_account", "no account");
+
+        assertEquals("Koichi Taura",authorName);
+        assertEquals("no account",facebookAccount);
+    }
 }
