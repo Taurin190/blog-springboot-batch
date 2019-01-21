303f431c1340462cb6a4117c53f390bc27f955fc
Koichi Taura <taura.koichi@gmail.com>
2019-01-21 20:54:39 +0900

    delete unnecessary class

--- src/main/java/com/taurin190/controller/BlogController.java
+++ src/main/java/com/taurin190/controller/BlogController.java
@@ -2,7 +2,6 @@ package com.taurin190.controller;
 
 import com.taurin190.entity.BlogEntity;
 import com.taurin190.service.BlogService;
-import com.taurin190.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -15,14 +14,10 @@ public class BlogController implements BatchController {
     @Autowired
     BlogService blogService;
 
-    @Autowired
-    TagService tagService;
-
     @Override
     public void exec() {
         List<BlogEntity> entityList = blogService.getBlogEntitiesByFiles();
         entityList.forEach(entity -> {
-//            tagService.saveOrUpdateByBlogEntity(entity);
             blogService.saveOrUpdateBlogEntity(entity);
         });
     }
