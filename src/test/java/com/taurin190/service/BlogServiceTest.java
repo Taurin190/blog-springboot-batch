9ea2ce30fbb3418a71f7616fd8e1062303bfb518
Koichi Taura <taura.koichi@gmail.com>
2019-02-04 21:43:35 +0900

    add empty test of blog service

---
+++ src/test/java/com/taurin190/service/BlogServiceTest.java
@@ -0,0 +1,47 @@
+package com.taurin190.service;
+
+import com.taurin190.entity.TagEntity;
+import com.taurin190.repository.BlogRepository;
+import com.taurin190.repository.TagRepository;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+
+public class BlogServiceTest {
+    @Mock
+    private BlogRepository blogRepository;
+
+    @Mock
+    private TagRepository tagRepository;
+
+    @Test
+    public void getBlogEntitiesByFiles() {
+
+    }
+
+    @Test
+    public void getAuthorEntityByRepository() {
+
+    }
+
+    @Test
+    public void saveOrUpdateTagEntity() {
+
+    }
+
+    @Test
+    public void saveOrUpdateTagEntityByName() {
+
+    }
+
+    @Test
+    public void getTagListByTagNameList() {
+
+    }
+
+    @Test
+    public void getTagEntityByName() {
+
+    }
+}
