d072e7518cfe2aaa267aa3f1b13b015af2016975
Koichi Taura <taura.koichi@gmail.com>
2019-02-04 21:56:11 +0900

    modify and add test related files

--- src/test/java/com/taurin190/service/BlogServiceTest.java
+++ src/test/java/com/taurin190/service/BlogServiceTest.java
@@ -1,12 +1,18 @@
 package com.taurin190.service;
 
+import com.taurin190.entity.BlogEntity;
 import com.taurin190.entity.TagEntity;
 import com.taurin190.repository.BlogRepository;
 import com.taurin190.repository.TagRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class BlogServiceTest {
     @Mock
@@ -15,9 +21,20 @@ public class BlogServiceTest {
     @Mock
     private TagRepository tagRepository;
 
+    @InjectMocks
+    private BlogService blogService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
     @Test
     public void getBlogEntitiesByFiles() {
+        List<BlogEntity> blogEntityList = blogService.getBlogEntitiesByFiles();
 
+        assertEquals(2, blogEntityList.size());
     }
 
     @Test
